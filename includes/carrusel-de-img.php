03ac5cca559fe0f682f9edbb3fbf23474d42c2e4
adilosa95 <adilovi95@gmail.com>
2019-11-01 12:04:51 +0100

    Visualización de pisos

--- includes/carrusel-de-img.php
+++ includes/carrusel-de-img.php
@@ -7,7 +7,6 @@ function getCarrusel( $img , $aCont)
     $carrousel .= '<img class="imgSlides" src="'.$img.'" alt="" srcset="">';
     $carrousel .= '<div class="items"><div class="left" onclick="siguienteImg(-1)">&#10094;</div>';
     $carrousel .= '<div class="right" onclick="siguienteImg('.count($aCont).')">&#10095;</div>';
-    $carrousel .= '<span class="circulo" onclick="thisImg(1)"></span>'; 
     $carrousel .= '</div>';
     $carrousel .= '</div>';
     echo $carrousel;
--- piso.php
+++ piso.php
@@ -6,8 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link rel="stylesheet" href="css/slider-images.css">
+    <link rel="stylesheet" href="css/widgets.css">
+    <link rel="stylesheet" href="css/estilos-piso.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 
 <body>
@@ -23,6 +25,27 @@
              print_r(getCarrusel($object, $a));
         }
      ?>
+     <div class="contenedor-izquierdo">
+         <div id="perfil">
+             <img id="user" src="img/isset/isset-user.png" alt="">
+             <h3>Nombre</h3>
+        </div>
+         <h2>Precio</h2>
+         <button class="button">Hola</button>
+     </div>
+     <div class="contenedor-derecho">
+         <h1>Calle</h1>
+         <div class="caracteristicas">
+            <p><img src="img/iconos-materiales/bandera.png" alt="habitación"> Ubicación</p>
+            <p><img src="img/iconos-materiales/visitas.png" alt="habitación"> Ubicación</p>
+            <p><img src="img/iconos-materiales/visitas.png" alt="habitación"> Ubicación</p>
+         </div>
+         <br>
+         <p>Lorem ipsum dolor sit amet consectetur adipiscing elit dignissim, facilisi netus metus nisl suscipit nec praesent scelerisque, justo porttitor fusce duis class id tempus. Mauris interdum orci pretium penatibus sociosqu vivamus at posuere et metus pharetra taciti erat, placerat fusce class id massa facilisi dapibus fermentum cursus nullam feugiat congue. Pharetra per elementum et feugiat vulputate mattis leo viverra mauris, duis lobortis tellus aliquam fusce euismod natoque nisi, sed venenatis ad quam pulvinar dictumst cubilia dui.
+Phasellus lectus eu vel mattis vitae nascetur hendrerit interdum, natoque vehicula euismod varius arcu nam nisi montes a, maecenas porta cum dignissim integer felis hac. Non commodo blandit aliquam orci habitant dictumst nibh iaculis magna pharetra, quis praesent cursus penatibus nisl magnis odio sociis ac, egestas leo tempus mattis donec montes varius convallis fringilla. Faucibus duis facilisi tempus blandit at venenatis vivamus elementum suscipit, taciti mus eget aliquet ultrices rhoncus orci proin non ullamcorper, cum odio iaculis laoreet dignissim per vitae sed.</p>
+         <br>
+         <h3>Comodidades</h3>
+</div>
 </body>
 <script src="js/slider.js"></script>
 
