f62ff75219a1f3a2f1d1580afec4d3f14af0041c
adilosa95 <adilovi95@gmail.com>
2019-11-07 19:58:22 +0100

    PhpStorm

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once(__DIR__."/includes/header.php");
 require_once(__DIR__."/includes/constantes.php");
+//
 $a = array(
     "widgets" => "widgets",
     "incio" => "estilos-inicio" 
