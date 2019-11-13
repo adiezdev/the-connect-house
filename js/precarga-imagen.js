4b84a40290cd9cc4425b2c297779636a37d904d6
adilosa95 <adilovi95@gmail.com>
2019-11-13 17:53:53 +0100

    Precarfar IMG

---
+++ js/precarga-imagen.js
@@ -0,0 +1,36 @@
+/**
+ * Funcion para visualizar la imagen de perfil antes de ser subida
+ *
+ * @param evt
+ */
+function precarfarImagen(evt) {
+    var files = evt.target.files;
+
+    for (var i = 0, f; f = files[i]; i++) {
+
+        if (!f.type.match('image.*')) {
+            continue;
+        }
+
+
+        var reader = new FileReader();
+
+        reader.onload = (function(theFile) {
+            return function(e) {
+                if (theFile.size <= 16000000) {
+                    //console.log(theFile.size);
+
+                    document.getElementById("imgperfil").src =  e.target.result;
+                    document.getElementById('hiddens').innerHTML += '<input type="hidden" name="imagen" id=' + theFile.name + ' value = ' + e.target.result + '>';
+                } else {
+                    alert("No pueden pesar mas de 16MB");
+                }
+
+            };
+        })(f);
+
+        reader.readAsDataURL(f);
+    }
+}
+
+document.getElementById('archivos').addEventListener('change', precarfarImagen, false);
